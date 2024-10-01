20940cd704aca082ad241f16ea4902f6565305c4
Sam ODonnell <samboy29@gmail.com>
2024-10-01 13:35:57 +1000

    add user#display_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
 
   has_many :phrases, dependent: :destroy
 
+  def display_name
+    return preferred_name if preferred_name.present?
+    return first_name if first_name.present?
+    return last_name if last_name.present?
+
+    "Anonymous"
+  end
+
   def phrase_categories
     @phrase_categories ||= phrases.select(:category).distinct.order(:category).pluck(:category)
   end
